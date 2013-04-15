e452c25cb17b8285d29b199bcb9b134e69e3ab53
Wayne Tsai <wayne@tsai.co>
2013-04-15 00:56:43 +0000

    Added slideshow plugin

--- scripts/burg.js
+++ scripts/burg.js
@@ -1,10 +1,44 @@
-jQuery(document).ready(function() {
+/* jQuery(document).ready(function() {
   jQuery(function($) {
-    $('.page-node #content form').superLabels({
+    $('.page-node #content form input').superLabels({
       duration:200,
     });
-    $('.page-panels #content form').superLabels({
+    $('.page-panels #content form input').superLabels({
       duration:200,
     });
   });
+});
+*/
+jQuery(window).ready(function() {
+/*  jQuery(function($) {
+    $("#carousel").carouFredSel({
+      width       : "100%",
+      scroll      : {
+        fx          : "crossfade"
+      },
+      items       : {
+        visible     : 1,
+        width       : 640,
+        height      : 320
+      }
+    });
+  }); 
+  jQuery("#carousel").carouFredSel({
+    height: 320,
+    align: "left",
+    width: 1280,
+    items: {
+      width: 1280,
+      height: 320
+    },
+    scroll: {
+      easing: "linear",
+      fx: "crossfade"
+    },
+    auto: {
+      timeoutDuration: 5000,
+      delay: 1000
+    }
+  });*/
+jQuery('#carousel').cycle();
 });
